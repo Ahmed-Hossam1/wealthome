b6c8a503f33ae1b99e2a8d0dfdcd10acc94ec73d
Ahmed-Hossam1 <108232062+Ahmed-Hossam1@users.noreply.github.com>
2024-08-08 00:25:52 +0300

    signup.js

--- signup.js
+++ signup.js
@@ -13,9 +13,15 @@ let signup_btn = document.getElementById("sign_up");
 let signup_form = document.getElementById("sign_up_form");
 let error = document.getElementById("error");
 
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
 signup_form.addEventListener("submit", (e) => {
   let errors = [];
-  if (name_input.value == "") {
+  if (name_input.value === "") {
     errors.push("Name required");
     name_input.classList.add("incorrect");
   } else {
@@ -25,6 +31,8 @@ signup_form.addEventListener("submit", (e) => {
   if (email_input.value === "") {
     errors.push("Email required");
     email_input.classList.add("incorrect");
+  } else if (!isValidEmail(email_input.value)) {
+    errors.push("Email not valid");
   } else {
     email_input.classList.remove("incorrect");
   }
@@ -32,9 +40,14 @@ signup_form.addEventListener("submit", (e) => {
   if (password_input.value === "") {
     errors.push("Password required");
     password_input.classList.add("incorrect");
+  } else if (password_input.value.length < 8) {
+    errors.push("Password must be at least 8 charcter");
+  } else if (password_input.value.length > 16) {
+    errors.push("Password must be at less than 16 charcter");
   } else {
     password_input.classList.remove("incorrect");
   }
+
   error.innerHTML = errors.join(". ");
 
   if (errors.length > 0) {
